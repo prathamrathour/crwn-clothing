c3e92d946ea49ccec28326314b0831ea8b54754a
Pratham Rathour <prathamrathour2018@gmail.com>
2021-09-06 15:47:38 +0530

    adding directory reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,20 @@
 import { combineReducers } from "redux";
 import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user.reducer";
+import { persistReducer } from "redux-persist";
 
-export default combineReducers({
+import storage from "redux-persist/lib/storage";
+import directoryReducer from "./directory/directory.reducer";
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
